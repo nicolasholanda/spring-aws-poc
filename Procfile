e245bd7e8a46297cfc0b5fdc0717544cfdecb1fe
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-05-23 20:22:54 -0300

    Added EB files and sample controller

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/spring-aws-poc.jar
---
+++ src/main/java/com/github/nicolasholanda/spring_aws_poc/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.spring_aws_poc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
