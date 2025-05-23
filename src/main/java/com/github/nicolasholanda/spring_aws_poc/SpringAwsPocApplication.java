bd280fe1f39c8a971cdb883730a663f0d6af28bd
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-05-22 22:39:31 -0300

    Initial commit

---
+++ src/main/java/com/github/nicolasholanda/spring_aws_poc/SpringAwsPocApplication.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.spring_aws_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nicolasholanda/spring_aws_poc/SpringAwsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nicolasholanda.spring_aws_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
